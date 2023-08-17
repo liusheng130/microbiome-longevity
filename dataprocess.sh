a7b7a1d51c12b43112bc3778565aa712ad22073c
liusheng130 <liusheng130@users.noreply.github.com>
2023-08-17 21:10:03 +0800

    Update and rename cleandata.sh to dataprocess.sh

--- dataprocess.sh
+++ dataprocess.sh
@@ -1,16 +1,24 @@
+## quality control of raw data
 for i in `cat sample_List.txt`;do echo "#!/bin/bash
 #PBS -N fastp
 fastp -i fq/$i/$i\_1.fastq.gz -o cleandata/$i.1.clean.fastq.gz -I fq/$i/$i\_2.fastq.gz -O cleandata/$i.2.clean.fastq.gz \
 -h cleandata/output/$i\.fastp.html" >>shell/$i.fastp.sh;
 done
-
 for i in `cat sample_List.txt`;do qsub shell/$i.fastp.sh; done
 
+## remove host sequences
 for i in `cat sample_List.txt`;do echo "#!/bin/bash
 #PBS -N bowtie2
 bowtie2 -p 4 -x /public1/data/liusheng/data/GRCh38/hg38 -1 cleandata/$i.1.clean.fastq.gz -2 cleandata/$i.2.clean.fastq.gz \
 --un-conc-gz rmhost/$i/$i.rmhost.fq.gz" >>shell/$i.rmhost.sh;
 done
-
 for i in `cat sample_List.txt`;do qsub shell/$i.rmhost.sh; done
+
+## assembly
+for i in `cat sample_List.txt`;do echo "#!/bin/bash
+#!/bin/bash
+#PBS -N megahit
+megahit -1 rmhost/$i/$i.rmhost_1.fq.gz -2 rmhost/$i/$i.rmhost_2.fastq.gz -o megahit/output/$i" >>shell/$i.megahit.sh;
+done
+for i in `cat sample_List.txt`;do qsub shell/$i.megahit.sh; done
 
