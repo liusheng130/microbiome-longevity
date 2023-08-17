4ba22a333b1d012cf385597c5e53d2ddd3cf56fb
liusheng130 <liusheng130@users.noreply.github.com>
2023-08-17 22:19:13 +0800

    Update dataprocess.sh

--- dataprocess.sh
+++ dataprocess.sh
@@ -74,6 +74,18 @@ done
 cat contig.bgc.result/*.bgc.TPM.sum.txt |awk '{print $1}' |sort |uniq >all.uniq.BGCs;
 perl mergy.TMP.pl allsample.BGCs.TPM.xls
 
+## calculate the species abundance using metaphlan
+for i in `cat sample_List.txt`;do echo "#!/bin/bash
+#PBS -N metaphlan
+metaphlan rmhost/$i/$i.rmhost_1.fq.gz,rmhost/$i/$i.rmhost_2.fq.gz --bowtie2db /data/data1/liusheng/database/metaphlan/mpa_vJan21/ \
+-x mpa_vJan21_CHOCOPhlAnSGB_202103 --nproc 4 --input_type fastq --bowtie2out $i.bowtie2.bz2 \
+-o metaphlan/output/$i.metagenome.txt" >>shell/$i.metaphlan.sh
+
+## mergy the species abundance of multiple samples
+merge_metaphlan_tables.py metaphlan/output/*metagenome.txt >merge.metagenome.txt
+sed '1'd merge.metagenome.txt |sed '1s/.metagenome//g' >merge.metagenome.xls
+awk '$0~/clade_name/||$0~/s__/{print $0}' merge.metagenome.xls |cut -d '|' -f 7- >merge.metagenome.species.xls
+sed 's/^s__//g' merge.metagenome.species.xls | sed '/|t__/'d |sed 's/clade_name//' >merge.metagenome.species.v1.xls
 
 
 
